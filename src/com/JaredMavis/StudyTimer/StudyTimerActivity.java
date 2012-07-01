ed9f5c5f0ee5e03eab460439e4f1d2aa98a0e16d
Jared Mavis <hellfire073@hotmail.com>
2012-07-01 15:51:15 -0700

    Added in initial project files.

---
+++ src/com/JaredMavis/StudyTimer/StudyTimerActivity.java
@@ -0,0 +1,13 @@
+package com.JaredMavis.StudyTimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StudyTimerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
